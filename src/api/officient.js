e2000b020a6c5a7d0d77a27ece028bd1e9f8f315
rubenberna <rubenmbernardes@gmail.com>
2018-08-21 16:38:32 +0200

    feat(): generate access token -- grant type not specified

--- src/api/officient.js
+++ src/api/officient.js
@@ -1,47 +1,54 @@
 import qs from 'qs';
-import axios from 'axios';
 
 const CLIENT_ID =	'504607';
-const ROOT_URL = 'https://app.officient.io';
+const ROOT_URL = 'https://app.officient.io/';
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 export default {
+  login() {
+    const querystring = {
+     client_id: CLIENT_ID,
+     state: 'xyz'
+   };
+   window.location = `${ROOT_URL}/authorize?${qs.stringify(querystring)}`;
+ },
   fetchPeople(token) {
     const request = require("request");
 
     const options = { method: 'GET',
-    url: 'https://api.officient.io/1.0/people/list',
-    qs: { page: '1' },
-    headers: {
-      Authorization: `Bearer ${token}`,
-    }
+      url: `${PROXY}${ROOT_URL}1.0/people/list`,
+      qs: { page: '1' },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
     };
 
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
-
       console.log(body);
     });
-
   }
 };
 
+// chromium-browser --disable-web-security --user-data-dir="[some directory here]"
+//
 // return axios.get(`${ROOT_URL}/1.0/people/list?page=1`, {
 //   headers: {
 //     Authorization: `Bearer ${token}`
 //   }
 // });
-
-
+//
+//
 // return axios.get(`${ROOT_URL}/1.0/people/list`, {
 //   qs: { page: '1'},
 //   headers: {
 //     Authorization: `Bearer ${token}`
 //   }
 // });
-
 //
-
-
+// //
+//
+//
 // var data = JSON.stringify(false);
 //
 // var xhr = new XMLHttpRequest();
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,7 +1,14 @@
 import api from '../../api/officient';
+import qs from 'qs';
 import { router } from '../../main';
 
-const TEMP_TOKEN = '038e9066e26741f73962c7cfe4e9c56f01831680';
+const CLIENT_ID =	'504607';
+const CLIENT_SECRET = 'DDSOnWNTuOZDNt4NJgPcG5vOup5uYM3NCOr5GDWUcOl685ScDP';
+const TOKEN_URL = 'https://api.officient.io/1.0/token';
+const GRANT_TYPE = 'authorization_code';
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+const TEMP_TOKEN = '05e4f86b8e8b5139e8f2495ba753098a0685a931';
 
 const state = {
   token: window.localStorage.getItem('officient_token')
@@ -12,11 +19,28 @@ const getters = {
 };
 
 const actions = {
-  login: ({ commit }, token) => {
-    commit('setToken', TEMP_TOKEN);
-    window.localStorage.setItem('officient_token', TEMP_TOKEN);
-    router.push('/');
-  },
+  login: () => {
+  api.login();
+},
+finalizeLogin({ commit }, search) {
+  const query = qs.parse(search.replace('?', ''));
+  const code = query.code;
+  const querystring = {
+    grant_type: 'authorization_code',
+    code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  };
+
+  const request = require("request");
+  const options = { method: 'POST',
+    url: 'https://cors-anywhere.herokuapp.com/https://api.officient.io/1.0/token',
+    body: `${qs.stringify(querystring)}`};
+   request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+     console.log(body);
+  });
+},
   logout: ({ commit }) => {
     commit('setToken', null);
     window.localStorage.removeItem('officient_token');
@@ -35,3 +59,21 @@ export default {
   actions,
   mutations
 };
+
+// login: ({ commit }, token) => {
+//   commit('setToken', TEMP_TOKEN);
+//   window.localStorage.setItem('officient_token', TEMP_TOKEN);
+//   router.push('/');
+// },
+
+
+code=8b2969e6fbae5f5d0dbf3bf24b44e3d39407ad2a&
+client_id=504607
+&client_secret=DDSOnWNTuOZDNt4NJgPcG5vOup5uYM3NCOr5GDWUcOl685ScDP&
+grant_type=authorization_code
+
+code=8b2969e6fbae5f5d0dbf3bf24b44e3d39407ad2a&
+client_id=504607&
+client_secret=DDSOnWNTuOZDNt4NJgPcG5vOup5uYM3NCOr5GDWUcOl685ScDP&
+grant_type=authorization_code&
+refresh_token='
