1d5127de74b6410d4915a5d52d187f61c1a2bdad
rubenberna <rubenmbernardes@gmail.com>
2018-08-23 23:16:44 +0200

    feat(): rendering avatar. need to use different menu to display info

--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -4,7 +4,35 @@
          v-if='isLoggedIn'>
          <appLoader :loading='loading'
          class="loader"/>
-         {{ person }}
+         <div class="person-detail-menu">
+           <img class="person-detail-menu-image ui medium circular image"
+           :src="person.avatar">
+
+           <div class="ui grid">
+            <div class="four wide column">
+              <div class="ui vertical fluid tabular menu">
+                <a class="item active">
+                  Bio
+                </a>
+                <a class="item">
+                  Pics
+                </a>
+                <a class="item">
+                  Companies
+                </a>
+                <a class="item">
+                  Links
+                </a>
+              </div>
+            </div>
+            <div class="twelve wide stretched column">
+              <div class="ui segment">
+                This is an stretched grid column. This segment will always match the tab height
+              </div>
+            </div>
+          </div>
+
+         </div>
     </div>
     <h2 v-else
       class="container">You're not logged in...</h2>
@@ -24,7 +52,21 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .person-detail {
+    margin-top: 100px;
+
+  }
+
+  .person-detail-menu {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .person-detail-menu-image.ui.medium.circular.image {
+    width: 334px;
+  }
+
   .loader {
     position: fixed;
     top: 50%;
