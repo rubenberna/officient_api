233cfd5fd8ccdb28d6b1a6cdbb8a58f27a3901d4
rubenberna <rubenmbernardes@gmail.com>
2018-08-24 12:50:33 +0200

    feat(): displays basic info per person

--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -8,44 +8,67 @@
            <img class="person-detail-menu-image ui medium circular image"
            :src="person.avatar">
 
-           <div class="ui grid">
-            <div class="four wide column">
-              <div class="ui vertical fluid tabular menu">
-                <a class="item active">
-                  Bio
-                </a>
-                <a class="item">
-                  Pics
-                </a>
-                <a class="item">
-                  Companies
-                </a>
-                <a class="item">
-                  Links
-                </a>
-              </div>
-            </div>
-            <div class="twelve wide stretched column">
-              <div class="ui segment">
-                This is an stretched grid column. This segment will always match the tab height
-              </div>
-            </div>
-          </div>
+           <sui-grid>
+              <sui-grid-column :width="4">
+                <sui-menu fluid vertical tabular>
+                  <a
+                    v-for="item in items"
+                    :key="item"
+                    is="sui-menu-item"
+                    :content="item"
+                    :active="isActive(item)"
+                    @click="select(item)"
+                  />
+                </sui-menu>
+              </sui-grid-column>
 
+              <sui-grid-column stretched :width="12">
+                <sui-segment>
+                  {{ itemInfo(person) }}
+
+                </sui-segment>
+              </sui-grid-column>
+            </sui-grid>
          </div>
     </div>
     <h2 v-else
-      class="container">You're not logged in...</h2>
+      class="container">
+      You're not logged in...</h2>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import Loader from './Loader.vue'
+import Loader from './Loader.vue';
+import SuiVue from 'semantic-ui-vue';
 
 export default {
   name: 'PersonDetail',
-  computed: mapGetters(['person', 'loading', 'isLoggedIn']),
+  data() {
+    return {
+      items: ['Bio', 'Contacts', 'Commute', 'Wages'],
+      active: 'Bio',
+    };
+  },
+  methods: {
+    isActive(name) {
+      return this.active === name;
+    },
+    select(name) {
+      this.active = name;
+    },
+    itemInfo(person) {
+      if (this.active === 'Bio') return`${person.name} started in ${person.current_role.start_date} as ${person.current_role.name}.`;
+      if (this.active === 'Contacts') return `Email: ${person.email} Phone: ${person.phone}`;
+    }
+  },
+  computed: {
+    ...mapGetters(['person', 'loading', 'isLoggedIn']),
+    gender(person) {
+      if (person.gender === 'female') return "she";
+      else "he";
+    }
+  },
   components: {
     appLoader: Loader
   }
@@ -65,6 +88,10 @@ export default {
 
   .person-detail-menu-image.ui.medium.circular.image {
     width: 334px;
+  }
+
+  .ui.segment {
+    width: 450px;
   }
 
   .loader {
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,10 @@ import Vuex from 'vuex';
 import Vue from 'vue';
 import auth from './modules/auth';
 import people from './modules/people';
+import SuiVue from 'semantic-ui-vue';
 
 Vue.use(Vuex);
+Vue.use(SuiVue);
 
 export default new Vuex.Store({
   modules: {
