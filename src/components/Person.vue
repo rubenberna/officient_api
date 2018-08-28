abe0503ed21a514281a8ddbf2821756a9491bf39
rubenberna <rubenmbernardes@gmail.com>
2018-08-28 14:42:10 +0200

    feat(): adapted getTransport method to return correct html

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -66,30 +66,33 @@ export default {
     },
     itemInfo(person, commute, schedule) {
       if (this.active === 'Bio')
-        return `<p>${person.name} started working with us ${this.getStartDate(person)} as ${person.current_role.name}.</p>
-        <p>${this.getGender(person)} and was born in ${this.getBirthday(person)} and is ${person.civil_state}.</p>`;
+        return `<p>${person.name} started working with us ${this.getStartDate(person)} as '${person.current_role.name}'.</p>
+        <p>${this.getGender(person)} was born in ${this.getBirthday(person)} and is ${person.civil_state}.</p>`;
 
       if (this.active === 'Contacts')
         return `<p> Email: ${person.email} </p>
         <p>Phone: ${person.phone} </p>`;
 
       if (this.active === 'Commute')
-      return `On a daily basis, ${person.name.split(' ')[0]} travels ${commute.distance.text} to come to work, with an average duration of ${commute.duration.text} per trip. The transports used may include: ${this.getTransport(commute)}. The weekly commute extends to ${this.getWeeklyCommute(commute)}`;
+      return `<p> On a daily basis, ${person.name.split(' ')[0]} travels ${commute.distance.text} to come to work, with an average duration of ${commute.duration.text} per trip. </p>
+      <p> ${person.name.split(' ')[0]} lives in ${person.address.city} and the transports used may include: ${this.getTransport(commute)}.</p>
+      <p>The weekly commute extends to ${this.getWeeklyCommute(commute)} hours.</p>`;
 
       if (this.active === 'Schedule') {
         let timetable = [];
         for (let day in schedule) {
           let hour = schedule[day] / 60;
-          timetable.push(`${day}: ${hour} hours`)
+          const dayCap = day.replace(/^\w/, c => c.toUpperCase());
+          timetable.push(`${dayCap}: ${hour} hours`)
         }
         const weekdays = timetable.splice(0, 5);
-        return `<ul>
-                  <li>${weekdays[0]}</li>
-                  <li>${weekdays[1]}</li>
-                  <li>${weekdays[2]}</li>
-                  <li>${weekdays[3]}</li>
-                  <li>${weekdays[4]}</li>
-                </ul>`
+      return `<ul style="list-style-type:none; padding-left:6px">
+                <li>${weekdays[0]}</li>
+                <li>${weekdays[1]}</li>
+                <li>${weekdays[2]}</li>
+                <li>${weekdays[3]}</li>
+                <li>${weekdays[4]}</li>
+              </ul>`
       }
     },
     getBirthday(person) {
@@ -110,13 +113,22 @@ export default {
           transports.push(entry.html_instructions);
         }
       }
-      return transports;
+      console.log(transports);
+      if (transports.length > 2) {
+        const last = transports.pop();
+        return `${transports.join(", ")} and ${last}`;
+      }
+      if (transports.length > 1) {
+        const last = transports.pop();
+        return `${transports} and ${last}`;
+      }
+      else transports;
     },
     getWeeklyCommute(commute) {
       const durationWeek = commute.duration.value * 10;
       const durationWeekText = moment().startOf('day')
         .seconds(durationWeek)
-        .format('h:mm:ss');
+        .format('h:mm');
       return durationWeekText;
     }
   },
